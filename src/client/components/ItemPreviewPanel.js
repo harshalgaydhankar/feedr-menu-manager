724f6479f0c2f8286c70dfbe50e896f5fb6a90d2
harshalgaydhankar <harshal.gaydhankar@axisbank.com>
2021-07-07 00:22:41 +0530

    Harshal | plug ItemPreview in ItemPreviewPanel

--- src/client/components/ItemPreviewPanel.js
+++ src/client/components/ItemPreviewPanel.js
@@ -1,22 +1,11 @@
 import React from 'react';
+import ItemPreview from "./ItemPreview";
 
 const ItemPreviewPanel = ({selectedItems}) => {
 
-    const getDietarySpan = (dietary, index) => {
-        return (
-            <span className="dietary" key={index}>{dietary}</span>
-        );
-    };
-
     const getItemPreview = (item) => {
         return (
-            <li className="item" key={item.id}>
-                <h2>{item.name}</h2>
-                <p>
-                    {item.dietaries.map((dietary, index) => getDietarySpan(dietary, index))}
-                </p>
-                <button className="remove-item">x</button>
-            </li>
+            <ItemPreview item={item}/>
         )
     };
 
